0c4e2c829bd67bb97ed2561f2103ec48fdeb312c
DiegoloM2 <diego.moreno.manes@hotmail.com>
2023-02-22 18:42:55 +0100

    Added test data

--- screens/Study.jsx
+++ screens/Study.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import View from "react-native";
 
+const TestQuestion = "What is Einsten's energy-mass equation?"
+const TestAnswer = "e = m x (c^2)"
+
 const Study = () => {
     return <View></View>
 }
