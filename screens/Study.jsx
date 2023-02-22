f5204a322b124c29ea748daee53c1e2b6d44da77
DiegoloM2 <diego.moreno.manes@hotmail.com>
2023-02-22 18:55:04 +0100

    Created StudyText component
    Created the component that stores the study text inu the study page.

--- screens/Study.jsx
+++ screens/Study.jsx
@@ -4,6 +4,11 @@ import View from "react-native";
 const TestQuestion = "What is Einsten's energy-mass equation?"
 const TestAnswer = "e = m x (c^2)"
 
+
+const StudyText = ({ children }) => {
+    return <Text variant = "bodyLarge">{ children }</Text>
+}
+
 const Study = () => {
     return <View></View>
 }
