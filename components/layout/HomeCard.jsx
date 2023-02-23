5914dbc0617f41cd45dbdaed9027986805e22f04
DiegoloM2 <diego.moreno.manes@hotmail.com>
2023-02-23 10:06:42 +0100

    added basic HomeCard styles

--- components/layout/HomeCard.jsx
+++ components/layout/HomeCard.jsx
@@ -1,6 +1,17 @@
 import React from "react";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 
+
+const styles = StyleSheet.create({
+    Container: {
+        marginHorizontal: 20,
+        marginTop: 15,
+        backgroundColor: "white",
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+        padding: 10
+    }
+})
 
 /**
  * A component that contains all the scrollable content of the homepage.
@@ -8,7 +19,7 @@ import { View } from "react-native";
 const HomeCard = (props) => {
 
     return (
-        <View>
+        <View style = {styles.Container}>
             { props.children }
         </View>
     )
