9d57fd3ae4ee0561fcd0708cdd82156e4fb5928a
DiegoloM2 <diego.moreno.manes@hotmail.com>
2023-02-24 08:55:43 +0100

    Styled the SearchBar

--- components/touchables/SearchBar.jsx
+++ components/touchables/SearchBar.jsx
@@ -1,5 +1,17 @@
 import React, { useState } from 'react';
+import { StyleSheet } from 'react-native';
 import { Searchbar} from 'react-native-paper';
+
+const styles = StyleSheet.create({
+  SearchBar: {
+    backgroundColor: "rgb(255, 255, 255)",
+    borderRadius: 30,
+    marginTop: 15,
+    borderWidth: 0.3,
+    borderColor: "rgba(155, 155, 155, 0.3)", 
+    marginHorizontal: 5,
+  }
+})
 
 const SearchBar = () => {
     const [searchQuery, setSearchQuery] = useState('');
@@ -11,9 +23,10 @@ const SearchBar = () => {
         placeholder="Search your decks here"
         onChangeText={onChangeSearch}
         value={searchQuery}
-        icon="search"
+        icon="cloud-search-outline"
         showloading={true}
         testID='searchbar'
+        style = {styles.SearchBar}
       />
     );
   };
