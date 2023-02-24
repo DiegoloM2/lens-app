455b0d755a3bbf606d0107c53b53ae46d8b3010e
Fernando Blanco <fblanco.alonso05@gmail.com>
2023-02-24 23:09:58 +0100

    NavBar actualizado

--- components/layout/NavBar.jsx
+++ components/layout/NavBar.jsx
@@ -1,25 +1,38 @@
 import * as React from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback } from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 
 import { Appbar, Avatar } from 'react-native-paper';
 
 export default function NavBar() {
   return (
-   <Appbar.Header>    
-    <Avatar.Text size={30} label="FB" color="green"/> 
+   <Appbar.Header style={styles.container}>    
+    <Avatar.Image size={30} source={require("../assets/Profile-Avatar.png")}/> 
     <Appbar.Content text = ""/>       
-    <Appbar.Action icon="fire-circle" />
+    <Appbar.Action icon="fire-circle" color="orange" />    
     <Text style={styles.text}>77</Text>
-    <Appbar.Action icon="bookshelf"  />
-    <Text style={styles.text}>10</Text>
+    <TouchableWithoutFeedback>
+    <MaterialCommunityIcons name="book-open-variant" size={25} color = "dark-grey"/>
+    </TouchableWithoutFeedback>    
+    <Text style={styles.text_}>10</Text>
    </Appbar.Header>
   );
 }
 
 const styles = ({
+   text_: {
+    color: "#fff",
+     fontSize: 15,
+     flex:0.2,
+     padding: 7
+   },
    text: {
     color: "#fff",
      fontSize: 15,
+     flex:0.2,
    },
+   container: {
+     justifyContent: "space-evenly",
+   }
 });
