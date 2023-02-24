8de5f5f9857505444c15e79a88f2c85a57ca273a
DiegoloM2 <diego.moreno.manes@hotmail.com>
2023-02-24 09:02:57 +0100

    Created TestDecks
    Created TestDecks for development

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -4,6 +4,27 @@ import AuthContext from "../contexts/AuthContext";
 import HomeCard from "../components/layout/HomeCard";
 import NavBar from "../components/layout/NavBar";
 
+const TestDecks = [
+    {
+        "name": "Physics",
+        "studiedCards": 15,
+        "remainingCards": 10,
+        "description": "Deck to study physics"
+    },
+    {
+        "name": "Maths",
+        "studiedCards": 1,
+        "remainingCards": 8,
+        "description": "Deck to study mathematics"
+    },
+    {
+        "name": "Literature",
+        "studiedCards": 13,
+        "remainingCards": 20,
+        "description": "Deck to study English Literature"
+    }    
+    
+]
 
 const Home = () => {
     const auth = useContext(AuthContext);
