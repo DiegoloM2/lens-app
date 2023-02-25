0c71da86ac52feb20a3426b5e35f45a5c119c9f1
DiegoloM2 <diego.moreno.manes@hotmail.com>
2023-02-25 09:02:05 +0100

    Fixed InputPassword showing password on default

--- components/forms/InputPassword.jsx
+++ components/forms/InputPassword.jsx
@@ -5,13 +5,13 @@ import { TextInput } from "react-native-paper";
 const InputPassword = (props) => {
     const [showPassword, setShowPassword] = useState(false)
     return (
-        <Input secureTextEntry = {showPassword} 
+        <Input secureTextEntry = {!showPassword} 
             label = "Password" 
             name = "password" 
             type = "password" 
             placeholder = "type your password"
             right={<TextInput.Icon icon = "eye" onPress = {(e) => {setShowPassword(!showPassword)}}/>}
-            left={<TextInput.Icon icon = "key" onPress = {(e) => {setShowPassword(!showPassword)}}/>}
+            left={<TextInput.Icon icon = "key" />}
             value = {props.value}
             onChangeText = {props.onChangeText}
             errors = {props.errors}
