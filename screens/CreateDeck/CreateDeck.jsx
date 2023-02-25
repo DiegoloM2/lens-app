0dd47feab79fa1b7fcd7e1ef8065f6cc690336f6
DiegoloM2 <diego.moreno.manes@hotmail.com>
2023-02-25 09:59:23 +0100

    Initial creation by ChatGpt

---
+++ screens/CreateDeck/CreateDeck.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ScrollView } from "react-native";
+import DeckForm from "./DeckForm";
+  
+
+export default CreateDeck = () => {
+    return <ScrollView>
+        <DeckForm />
+    </ScrollView>
+}
---
+++ screens/CreateDeck/DeckForm.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#fff",
+  },
+  label: {
+    marginBottom: 5,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  input: {
+    height: 40,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+    fontSize: 16,
+  },
+  error: {
+    color: "red",
+    marginBottom: 5,
+  },
+  button: {
+    backgroundColor: "#007aff",
+    borderRadius: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
+
+const DeckForm = ({ initialValues = { name: "", description: "", parent_deck: "" }, onSubmit }) => {
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required("Name is required"),
+    description: Yup.string().required("Description is required"),
+    parent_deck: Yup.string().required("Parent deck is required"),
+  });
+
+  return (
+    <View style={styles.container}>
+      <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
+        {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
+          <>
+            <Text style={styles.label}>Name</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={handleChange("name")}
+              onBlur={handleBlur("name")}
+              value={values.name}
+            />
+            {touched.name && errors.name && <Text style={styles.error}>{errors.name}</Text>}
+            <Text style={styles.label}>Description</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={handleChange("description")}
+              onBlur={handleBlur("description")}
+              value={values.description}
+              multiline={true}
+            />
+            {touched.description && errors.description && (
+              <Text style={styles.error}>{errors.description}</Text>
+            )}
+            <Text style={styles.label}>Parent Deck</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={handleChange("parent_deck")}
+              onBlur={handleBlur("parent_deck")}
+              value={values.parent_deck}
+            />
+            {touched.parent_deck && errors.parent_deck && (
+              <Text style={styles.error}>{errors.parent_deck}</Text>
+            )}
+            <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+              <Text style={styles.buttonText}>Create Deck</Text>
+            </TouchableOpacity>
+          </>
+        )}
+      </Formik>
+    </View>
+  );
+};
+
+export default DeckForm;
