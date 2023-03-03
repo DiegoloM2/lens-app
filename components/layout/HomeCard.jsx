346119c887ef607b0f409431ea14e030e4e1b758
DiegoloM2 <diego.moreno.manes@hotmail.com>
2023-03-03 20:30:58 +0100

    Changed homepage styles

--- components/layout/HomeCard.jsx
+++ components/layout/HomeCard.jsx
@@ -1,37 +1,37 @@
 import React from "react";
 import { ScrollView, StyleSheet, View } from "react-native";
-import { Text } from "react-native-paper";
+import { Text, useTheme } from "react-native-paper";
 import ProgressBar from "../displays/ProgressBar";
 import SearchBar from "../touchables/SearchBar";
 import DeckTitle from "../layout/DeckTitle";
 
-const styles = StyleSheet.create({
-    Container: {
-        marginHorizontal: 20,
-        marginTop: 15,
-        backgroundColor: "white",
-        borderTopLeftRadius: 20,
-        borderTopRightRadius: 20,
-        padding: 10
-    },
-    ProgBar: {
-        width: '60%',
-        borderWidth: 1, 
-        borderColor: "blue",
-        height: 160,
-        marginLeft: "20%"
-    }
-})
 
 /**
  * A component that contains all the scrollable content of the homepage.
  */
 const HomeCard = (props) => {
+    const theme = useTheme();
+    const styles = StyleSheet.create({
+        Container: {
+            marginHorizontal: 20,
+            marginTop: 15,
+            padding: 10
+        },
+        DeckContainer: {
+            marginTop: 20,
+            padding: 10,
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+
+          backgroundColor: theme.colors.background,
+        }
+    })
+    
 
     return (
         <ScrollView style = {styles.Container}>
             <ProgressBar />
-            <View style = {styles.Container}>
+            <View style = {styles.DeckContainer}>
                 <SearchBar />
                 { props.decks.map((deck, idx) => <DeckTitle deck = {deck} key = {idx}/>)}
                 { props.children }
