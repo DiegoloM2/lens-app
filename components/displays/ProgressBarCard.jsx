9688d8135a1f8960afebcf1c648efae4da6513f2
Diego Moreno Mañes <diego.moreno.manes@hotmail.com>
2023-03-08 19:55:28 +0100

    advanced styling progressBar

--- components/displays/ProgressBarCard.jsx
+++ components/displays/ProgressBarCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Avatar, Button, Card, Text } from 'react-native-paper';
 import { StyleSheet, TouchableOpacity } from "react-native";
 import ProgressBar from "./ProgressBar";
+import { useNavigation } from "@react-navigation/native";
 
 const LeftContent = props => <Avatar.Icon {...props} color="white" icon="cards-outline" />
 
@@ -9,6 +10,8 @@ const LeftContent = props => <Avatar.Icon {...props} color="white" icon="cards-o
 
 
 const ProgressBarCard = () => {
+    const navigator = useNavigation();
+
     const styles = StyleSheet.create({
         Card: {
             width: "87%",
@@ -17,23 +20,36 @@ const ProgressBarCard = () => {
         },
         progBar: {
             alignSelf: "center",
+            marginTop: 10
         },
-        title: {
-            fontSize: 50
+        button: {
+            marginTop: 10
+        },
+        buttonText: {
+            fontSize: 18
+        },
+        cardSubtitle: {
+            fontSize: 11,
+            color: "rgba(150,150,150,1)"
         }
     })
 
 
     return (
         <Card style = {styles.Card}>
-        <Card.Title title="Today" left={LeftContent} style = {styles.title}/>
+        <Card.Title 
+            title= "Today" 
+            subtitle = "remaining cards = objective - studied today"
+            subtitleStyle = {styles.cardSubtitle}
+            left={LeftContent} 
+            titleVariant = "headlineSmall"/>
         <Card.Content>
                 <ProgressBar style = {styles.progBar }/>
         </Card.Content>
         
         <Card.Actions>
-            <TouchableOpacity>
-                <Button mode = "contained">Keep going</Button>
+            <TouchableOpacity onPress = {() => {navigator.navigate("Study")}}>
+                <Button mode = "contained" style = {styles.button} labelStyle = {styles.buttonText}>Keep going</Button>
             </TouchableOpacity>
         </Card.Actions>
         </Card>
