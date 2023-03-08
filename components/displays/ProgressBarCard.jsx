dd5dc304d2f17ffecaf9783daa519f0bc624a724
Diego Moreno Mañes <diego.moreno.manes@hotmail.com>
2023-03-08 19:32:28 +0100

    ProgressBarCard styled basically

--- components/displays/ProgressBarCard.jsx
+++ components/displays/ProgressBarCard.jsx
@@ -1,23 +1,39 @@
 import React from "react";
 import { Avatar, Button, Card, Text } from 'react-native-paper';
+import { StyleSheet, TouchableOpacity } from "react-native";
 import ProgressBar from "./ProgressBar";
 
 const LeftContent = props => <Avatar.Icon {...props} color="white" icon="cards-outline" />
 
 
-const ProgressBarCard = () => (
-    <Card>
-      <Card.Title title="Decks Progress" subtitle="Let's work" left={LeftContent} />
-      <Card.Content>
-        <Text> </Text>{/*Espacio entre titulo y ProgressBar*/}
-        <ProgressBar />
-  
-      </Card.Content>
-    
-      <Card.Actions>
-        <Button color="blue">Pussy</Button>
-        <Button color="blue">Keep going</Button>
-      </Card.Actions>
-    </Card>
-  );
+
+
+const ProgressBarCard = () => {
+    const styles = StyleSheet.create({
+        Card: {
+            width: "87%",
+            marginHorizontal: "6.5%",
+            marginTop: 50
+        },
+        progBar: {
+            alignSelf: "center",
+        },
+    })
+
+
+    return (
+        <Card style = {styles.Card}>
+        <Card.Title title="Today" left={LeftContent} />
+        <Card.Content>
+                <ProgressBar style = {styles.progBar }/>
+        </Card.Content>
+        
+        <Card.Actions>
+            <TouchableOpacity>
+                <Button mode = "contained">Keep going</Button>
+            </TouchableOpacity>
+        </Card.Actions>
+        </Card>
+    )
+    };
 export default ProgressBarCard;
