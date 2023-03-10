e069a50c5f915632df0905d9fa2e93cd2b8c522a
DiegoloM2 <diego.moreno.manes@hotmail.com>
2023-03-10 15:05:02 +0100

    added basic styling to Deck

--- screens/Deck.jsx
+++ screens/Deck.jsx
@@ -1,10 +1,32 @@
 import React from "react";
-import { View } from "react-native";
+import { View, ScrollView, StyleSheet } from "react-native";
+import DeckCard from "../components/displays/DeckCard";
 import NavBar from "../components/layout/NavBar";
+import { TestDecks } from "../utils/testData.jsx";
 
+
+const styles = StyleSheet.create({
+    deckCardsContainer: {
+        marginTop:15,
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-around",
+    },
+    deckCard: {
+        flex: 1,
+        margin: 15,
+        height: 130
+    }
+})
 
 export default function Deck () {
     return <View>
         <NavBar />
+
+        <ScrollView>
+            <View style = {styles.deckCardsContainer}>
+                { TestDecks.slice(0,2).map((deck, idx) => <DeckCard deck = {deck} key = {idx} style = {styles.deckCard}/>)}
+            </View>
+        </ScrollView>
     </View>
 }
