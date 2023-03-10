75ea07c7e2b7df6060f3ef405e1dee2b019ec350
DiegoloM2 <diego.moreno.manes@hotmail.com>
2023-03-10 14:41:12 +0100

    created a testData file to use mockups throughout proj

---
+++ utils/testData.jsx
@@ -0,0 +1,21 @@
+export const TestDecks = [
+    {
+        "name": "Physics",
+        "studiedCards": 15,
+        "remainingCards": 10,
+        "description": "Deck to study physics"
+    },
+    {
+        "name": "Maths",
+        "studiedCards": 1,
+        "remainingCards": 8,
+        "description": "Deck to study mathematics"
+    },
+    {
+        "name": "Literature",
+        "studiedCards": 13,
+        "remainingCards": 20,
+        "description": "Deck to study English Literature"
+    }    
+    
+]
