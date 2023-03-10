c9287afaa6a23418cdfbfae7e3ad0f95de8575aa
DiegoloM2 <diego.moreno.manes@hotmail.com>
2023-03-10 14:55:36 +0100

    Updated deck so it contains deckCards within it

--- screens/Deck.jsx
+++ screens/Deck.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { View } from "react-native";
 import NavBar from "../components/layout/NavBar";
 
+
 export default function Deck () {
     return <View>
         <NavBar />
