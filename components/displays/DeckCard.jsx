0a6b64998753a801e905e82db44fac5a0911679b
DiegoloM2 <diego.moreno.manes@hotmail.com>
2023-03-10 15:34:00 +0100

    Styled DeckCard

--- components/displays/DeckCard.jsx
+++ components/displays/DeckCard.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Avatar, Button, Card, Text } from 'react-native-paper';
+import { StyleSheet, TouchableOpacity } from 'react-native';
 
-const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
 /**
  * This card displays a deck in card form
  * for the "most recent" section of the Decks page.
@@ -9,16 +9,28 @@ const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
  * @param { StyleSheet } style - the style to be applied to the card.  
  * @returns 
  */
-const DeckCard = (props) => (
-  <Card style = {props.style}>
-    <Card.Title title={props.deck.name} subtitle = {props.deck.lastStudied} left={LeftContent} />
-    <Card.Content>
-        <Text>{ props.deck.description }</Text>
-    </Card.Content>
-    <Card.Actions>
-      <Button>Study</Button>
-    </Card.Actions>
-  </Card>
-);
+const DeckCard = (props) => {
+    const card = StyleSheet.create({
+        card: {
+            aspectRatio: 1,
+            width: "100%",
+            borderRadius: 7
+        }
+    })
+
+    return (
+        <TouchableOpacity style = {props.style}>
+            <Card style = {card}>
+                <Card.Title title={props.deck.name} subtitle = {props.deck.lastStudied} />
+                <Card.Content>
+                    <Text>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                        Eaque, laudantium nisi?
+                    </Text>
+                </Card.Content>
+            </Card>
+        </TouchableOpacity>
+    )
+}
 
 export default DeckCard;
