230f359b430d51a8f86f0a7d64be908590d7ca60
DiegoloM2 <diego.moreno.manes@hotmail.com>
2023-03-14 10:09:24 +0100

    styled deckCards container

--- screens/Deck.jsx
+++ screens/Deck.jsx
@@ -3,21 +3,33 @@ import { View, ScrollView, StyleSheet, Text } from "react-native";
 import DeckCard from "../components/displays/DeckCard";
 import NavBar from "../components/layout/NavBar";
 import { TestDecks } from "../utils/testData.jsx";
+import { shadowStyle } from "../utils/styles";
+import SearchBar from "../components/touchables/SearchBar";
 
 
 const styles = StyleSheet.create({
     deckCardsContainer: {
-        marginVertical: 10,
-        marginHorizontal: 5,
         flex: 1,
         flexDirection: "row",
         justifyContent: "space-around",
-        borderRadius: 15,
-        elevation: 10
+        marginBottom: 10
     },
     deckCard: {
         flex: 1,
+        marginHorizontal: 3,
+    },
+    container: {
+        marginVertical: 10,
         marginHorizontal: 7,
+        padding: 10,
+        borderRadius: 15,
+    },
+    containerTitle: {
+        color: "rgba(0,0,0,0.7)",
+        fontWeight: "bold",
+        fontSize: 13,
+        marginBottom: 5,
+        marginLeft: 5,
     }
 })
 
@@ -26,10 +38,22 @@ export default function Deck () {
         <NavBar />
 
         <ScrollView>
-            <Text>Most recent</Text>
-            <View style = {styles.deckCardsContainer}>
-                { TestDecks.slice(0,).map((deck, idx) => <DeckCard deck = {deck} key = {idx} style = {styles.deckCard}/>)}
+            <SearchBar />
+            <View style = {[styles.container, shadowStyle]}>
+                <Text style = {styles.containerTitle}>Recent decks</Text>
+                <View style = {styles.deckCardsContainer}>
+                { TestDecks.slice(0,3).map((deck, idx) => <DeckCard deck = {deck} key = {idx} style = {styles.deckCard}/>)}
+                </View>
+
+                <Text style = {styles.containerTitle}>Other decks</Text>
+
+
+                { TestDecks.slice(3,).map((deck, idx) => <Text deck = {deck} key = {idx} style = {styles.deckCard}>{deck.name}</Text>)}
+                
             </View>
+
+
+
         </ScrollView>
     </View>
 }
