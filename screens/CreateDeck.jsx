d6455d9fba76641d37c28d476b792667b52594a5
Matheus <matheustran@gmail.com>
2023-03-14 21:11:27 +0100

    changed the styling of build a deck

--- screens/CreateDeck.jsx
+++ screens/CreateDeck.jsx
@@ -1,15 +1,22 @@
 import React from "react";
-import { ScrollView, View } from "react-native";
+import { ScrollView, View, StyleSheet } from "react-native";
 import NavBar from "../components/layout/NavBar";
 import DeckForm from "../components/forms/DeckForm";
   
 
 export default CreateDeck = () => {
+  const styles = StyleSheet.create({
+    Container: {
+        backgroundColor: "white",
+        height:"100%"
+    }
+  })
     return (
       <View>
         <NavBar />
-        <ScrollView>
+        <ScrollView style = {styles.Container}>
             <DeckForm />
         </ScrollView>
-      </View>)
+      </View>
+    )
 }
