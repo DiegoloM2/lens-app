1e0b05b9cadc9a5be63c88af3f832087cc3deb94
Fernando Blanco <fblanco.alonso05@gmail.com>
2023-03-15 18:59:00 +0100

    Positioning the ProgressBar

--- components/layout/CardDiv.jsx
+++ components/layout/CardDiv.jsx
@@ -1,32 +1,35 @@
 import React from "react";
-import {View, Text} from "react-native";
+import { View, Text } from "react-native";
 
 import CardList from "./CardList";
-import  ProgressBar  from "../displays/ProgressBar";
-
+import ProgressBar from "../displays/ProgressBar";
 
 export default function CardDiv() {
-
-    return (
-
-        <View style={styles.container}>
-        <ProgressBar style={styles.progressbar}/>
-        <CardList/>
-        </View>
-
-
-    );
-
+  return (
+    <View style={styles.container}>
+      <ProgressBar style={styles.progressbar} size={100} />
+      <Text style={styles.text}>Cards</Text>
+      <CardList />
+    </View>
+  );
 }
-
 
 const styles = {
-container: {
-backgroundColor: "#E9E4E2",
-top: 50,
+  container: {
+    backgroundColor: "#E9E4E2",
+    top: 50,
+    borderRadius: 30,
+    padding: 10,
+  },
+  progressbar: {
+    top: 15,
+    left: 240,
+  },
 
-},
-progressbar: {
-    top: 10,
-}
-}
+  text: {
+    top: 15,
+    left: 10,
+    fontSize: 18,
+    color: "grey",
+  },
+};
--- components/layout/CardList.jsx
+++ components/layout/CardList.jsx
@@ -35,6 +35,6 @@ export default function CardList() {
 
 const styles = {
   flatlist: {
-    marginTop: 50,
+    marginTop: 15,
   },
 };
