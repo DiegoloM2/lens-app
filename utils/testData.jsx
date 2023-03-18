a6aa28bad22cd6185ffada62e7b56cc1170b91e2
DiegoloM2 <diego.moreno.manes@hotmail.com>
2023-03-18 19:36:17 +0100

    Updated TestData to use "title" instead of "name" for decks

--- utils/testData.jsx
+++ utils/testData.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 export const TestDecks = [
     {
-        "name": "Physics",
+        "title": "Physics",
         "studiedCards": 15,
         "remainingCards": 10,
         "description": "Deck to study physics",
         "lastStudied": "2022-11-02"
     },
     {
-        "name": "Maths",
+        "title": "Maths",
         "studiedCards": 1,
         "remainingCards": 8,
         "description": "Deck to study mathematics",
@@ -16,49 +16,49 @@ export const TestDecks = [
 
     },
     {
-        "name": "Literature",
+        "title": "Literature",
         "studiedCards": 13,
         "remainingCards": 20,
         "description": "Deck to study English Literature and shakespearean exonomics",
         "lastStudied": "2022-11-02",
     },
     {
-        "name": "Literature",
+        "title": "Literature",
         "studiedCards": 13,
         "remainingCards": 20,
         "description": "Deck to study English Literature and shakespearean exonomics",
         "lastStudied": "2022-11-02",
     },
     {
-        "name": "Literature",
+        "title": "Literature",
         "studiedCards": 13,
         "remainingCards": 20,
         "description": "Deck to study English Literature and shakespearean exonomics",
         "lastStudied": "2022-11-02",
     },
     {
-        "name": "Literature",
+        "title": "Literature",
         "studiedCards": 13,
         "remainingCards": 20,
         "description": "Deck to study English Literature and shakespearean exonomics",
         "lastStudied": "2022-11-02",
     },
     {
-        "name": "Literature",
+        "title": "Literature",
         "studiedCards": 13,
         "remainingCards": 20,
         "description": "Deck to study English Literature and shakespearean exonomics",
         "lastStudied": "2022-11-02",
     },
     {
-        "name": "Literature",
+        "title": "Literature",
         "studiedCards": 13,
         "remainingCards": 20,
         "description": "Deck to study English Literature and shakespearean exonomics",
         "lastStudied": "2022-11-02",
     },
     {
-        "name": "Literature",
+        "title": "Literature",
         "studiedCards": 13,
         "remainingCards": 20,
         "description": "Deck to study English Literature and shakespearean exonomics",
