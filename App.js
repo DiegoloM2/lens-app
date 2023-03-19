2cacc32fc6d15633839e5fa55c9494cb96fafcfd
DiegoloM2 <diego.moreno.manes@hotmail.com>
2023-03-19 08:56:12 +0100

    changed navigator so can only enter app if logged in.

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import BottomNav from "./components/layout/BottomNav";
@@ -5,10 +6,26 @@ import { AuthProvider } from './contexts/AuthContext';
 import { MD3LightTheme as DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import Register from "./screens/Register";
 import Login from "./screens/Login";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 
 
 const Stack = createNativeStackNavigator();
+const AuthStack = createNativeStackNavigator();
+
+
+const StackNavigator = () => (
+    <Stack.Navigator>
+      <Stack.Screen name="BottomNav" component={BottomNav} options = {{headerShown: false}} />
+    </Stack.Navigator>
+)
+
+const AuthStackNavigator = () => (
+  <AuthStack.Navigator>
+    <Stack.Screen name = "Login" component = {Login} options = {{headerShown:false}} />
+    <Stack.Screen name="Register" component={Register} options = {{headerShown: false}} />
+  </AuthStack.Navigator>
+)
 
 const theme = {
   ...DefaultTheme,
@@ -20,18 +37,42 @@ const theme = {
   },
 };
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <PaperProvider theme = { theme }>
+//       <NavigationContainer >
+//         <AuthProvider>
+//         </AuthProvider>
+//       </NavigationContainer>
+//     </PaperProvider>
+//   );
+// }
+
+
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // Simulate checking the user's authentication status on app start
+  useEffect(() => {
+    const checkAuthStatus = async () => {
+      const token = await AsyncStorage.getItem("token");
+      console.log(token)
+      const isAuthenticated = token !== null; // Replace with your authentication logic
+      setIsLoggedIn(isAuthenticated);
+    };
+
+    checkAuthStatus();
+  }, []);
+
   return (
-    <PaperProvider theme = { theme }>
-      <NavigationContainer >
-        <AuthProvider>
-          <Stack.Navigator>
-            <Stack.Screen name="BottomNav" component={BottomNav} options = {{headerShown: false}} />
-            <Stack.Screen name = "Login" component = {Login} options = {{headerShown:false}} />
-            <Stack.Screen name="Register" component={Register} options = {{headerShown: false}} />
-          </Stack.Navigator>
-        </AuthProvider>
-      </NavigationContainer>
+    <PaperProvider theme = {theme}>
+    <NavigationContainer>
+      <AuthProvider>
+        {isLoggedIn ? <StackNavigator /> : <AuthStackNavigator />}
+      </AuthProvider>
+    </NavigationContainer>
     </PaperProvider>
   );
-}
+};
+
+export default App;
