411d3e5a4237addfcce632604cc50442b7618cd1
DiegoloM2 <diego.moreno.manes@hotmail.com>
2023-03-19 12:06:50 +0100

    Added logout button to NavBar

--- components/layout/NavBar.jsx
+++ components/layout/NavBar.jsx
@@ -1,21 +1,46 @@
-import * as React from 'react';
+import React, { useContext, useState } from 'react';
 import { Text, View } from 'react-native';
+import { Menu } from "react-native-paper";
 import { useNavigation } from '@react-navigation/native';
+import AuthContext from "../../contexts/AuthContext"
 
 
 import { Appbar, Avatar } from 'react-native-paper';
+
+
+const ProfileMenu = () => {
+  const auth = useContext(AuthContext);
+  const [visible, setVisible] = useState(false);
+
+  const openMenu = () => setVisible(true);
+  const closeMenu = () => setVisible(false);
+
+  return (
+    <Menu 
+    visible={visible}
+    onDismiss={closeMenu}
+    anchor={<Appbar.Action size={33} color = "white" icon="alien" style={styles.avatar} onPress = {() => openMenu()}/> }
+    >
+      <Menu.Item onPress={() => {auth.logoutUser(); closeMenu();}} title="Logout" />
+
+    </Menu>
+  )
+
+}
+
 
 export default function NavBar() {
   const navigation = useNavigation();
   return (
   <View>
    <Appbar.Header style={styles.container}>    
-   <Avatar.Icon size={37} icon="alien" style={styles.avatar}/> 
-    <Appbar.Content text = ""/>       
-    <Appbar.Action icon="fire-circle" color="orange" style = {styles.icon} onPress={() => alert("Cooming soon! Be patient")} />    
-    <Text style={styles.text}>77</Text>
-    <Appbar.Action icon="book-open-variant" style = {styles.icon} onPress={() => navigation.navigate("Study")} />  
-    <Text style={styles.text}>10</Text>
+    <ProfileMenu />
+    <View style = {{flexDirection: "row", alignItems: "center", justifyContent: 'flex-end'}}>
+      <Appbar.Action icon="fire-circle" color="orange" style = {styles.icon} onPress={() => alert("Cooming soon! Be patient")} />    
+      <Text style={styles.text}>77</Text>
+      <Appbar.Action icon="book-open-variant" style = {styles.icon} onPress={() => navigation.navigate("Study")} />  
+      <Text style={styles.text}>10</Text>
+    </View>
    </Appbar.Header>
    </View>
   );
@@ -32,10 +57,12 @@ const styles = ({
     fontWeight: "bold"
   },
   container: {
-    justifyContent: "space-evenly",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
   },
   avatar: {
-   left: 10,
+   backgroundColor: "blue",
+
   },
   icon: {
     margin: 0
