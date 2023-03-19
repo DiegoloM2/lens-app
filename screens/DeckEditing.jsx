2125e9fced13c0bce6585a3140641d662419ab2d
DiegoloM2 <diego.moreno.manes@hotmail.com>
2023-03-19 03:39:54 +0100

    Added LinkSearchBar to DeckEditing Page

--- screens/DeckEditing.jsx
+++ screens/DeckEditing.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { View, FlatList, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import SearchBar from "../components/forms/SearchBar";
+import LinkSearchBar from "../components/touchables/LinkSearchBar";
 import DeckInfoEditing from "../components/forms/DeckInfoEditing";
 import CardDiv from "../components/layout/CardDiv";
+import { useNavigation } from "@react-navigation/core";
+import { TestDecks } from "../utils/testData";
 
 export default function DeckEditing() {
+  const navigator = useNavigation();
   return (
     <SafeAreaView style={styles.container}>
-      <SearchBar placeholder={"Search your cards here"} />
+      <LinkSearchBar placeholder={"Search your cards here"} onPress = {() => {navigator.navigate("Search", {availableDecks: TestDecks})} } />
       <DeckInfoEditing />
       <CardDiv />
     </SafeAreaView>
