1cf5640af91479a343229782bc1c56c09ce37e16
DiegoloM2 <diego.moreno.manes@hotmail.com>
2023-03-19 15:36:10 +0100

    Updated Deck  class

--- store/Deck.js
+++ store/Deck.js
@@ -1,10 +1,9 @@
 export default class Deck {
-    constructor(id, title, description, owner, cards = [], parentDeck = null) {
+    constructor(id, title, description, owner, parentDeck = null) {
       this.id = id;
       this.title = title;
       this.description = description;
       this.owner = owner;
-      this.cards = cards;
       this.parentDeck = parentDeck;
     }
 };
