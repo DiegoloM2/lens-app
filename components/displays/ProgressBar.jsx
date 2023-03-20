4df113fcd5a2ab300a97f01bd48e13226c77bf2a
DiegoloM2 <diego.moreno.manes@hotmail.com>
2023-03-20 12:14:10 +0100

    ProgressBar in home displays a random value

--- components/displays/ProgressBar.jsx
+++ components/displays/ProgressBar.jsx
@@ -3,20 +3,20 @@ import { AnimatedCircularProgress } from 'react-native-circular-progress';
 import { Text } from "react-native"
 
 // https://www.npmjs.com/package/react-native-circular-progress 
-const ProgressBar = (props) => (
-  <AnimatedCircularProgress
-    size={props.size}
-    width={5}
-    fill={59}
-    tintColor="blue"
-    backgroundColor="rgba(150,150,150,0.1)" 
-    rotation="0"
-    lineCap="round"
-    backgroundWidth="25" {...props}>
-      {(fill) => <Text style = {{color: "rgba(150,150,150,1)", fontSize: 25}}>{`${Math.round(fill)}%`}</Text>}
+const ProgressBar = (props) => {
+  return   <AnimatedCircularProgress
+  size={props.size}
+  width={5}
+  fill={Math.round(props.fill)}
+  tintColor="blue"
+  backgroundColor="rgba(150,150,150,0.1)" 
+  rotation="0"
+  lineCap="round"
+  backgroundWidth="25" {...props}>
+    {(fill) => <Text style = {{color: "rgba(150,150,150,1)", fontSize: 25}}>{`${Math.round(fill)}%`}</Text>}
 
-    </AnimatedCircularProgress>
-)
+  </AnimatedCircularProgress>; 
+}
 
 
 
--- components/displays/ProgressBarCard.jsx
+++ components/displays/ProgressBarCard.jsx
@@ -3,6 +3,7 @@ import { Avatar, Button, Card, Text } from 'react-native-paper';
 import { StyleSheet, TouchableOpacity } from "react-native";
 import ProgressBar from "./ProgressBar";
 import { useNavigation } from "@react-navigation/native";
+import { useCards } from "../../contexts/CardsContext";
 
 const LeftContent = props => <Avatar.Icon {...props} color="white" icon="cards-outline" />
 
@@ -11,7 +12,8 @@ const LeftContent = props => <Avatar.Icon {...props} color="white" icon="cards-o
 
 const ProgressBarCard = () => {
     const navigator = useNavigation();
-
+    const { cardsToStudyToday } = useCards();
+    const studyToday = cardsToStudyToday.length;
     const styles = StyleSheet.create({
         Card: {
             width: "87%",
@@ -44,7 +46,7 @@ const ProgressBarCard = () => {
             left={LeftContent} 
             titleVariant = "headlineSmall"/>
         <Card.Content>
-                <ProgressBar style = {styles.progBar } size = {150}/>
+                <ProgressBar style = {styles.progBar } size = {150} fill = {studyToday == 0 ? 100: 100 * (Math.random() * studyToday / studyToday)}/>
         </Card.Content>
         
         <Card.Actions>
