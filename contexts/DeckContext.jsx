43d522caefd46bfb45a2c4a651656bd7d4e9029f
DiegoloM2 <diego.moreno.manes@hotmail.com>
2023-03-21 10:59:57 +0100

    Fixed deckContext constant re-rendering.

--- contexts/DeckContext.jsx
+++ contexts/DeckContext.jsx
@@ -1,32 +1,39 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
-import { getUserDecks } from '../store/storage';
+import { getUserDecks, saveDeck } from '../store/storage';
 import AuthContext from './AuthContext';
 
 const DecksContext = createContext();
 
 export const DecksProvider = ({ children }) => {
-    const auth  = useContext(AuthContext);
+    const auth = useContext(AuthContext);
     const [decks, setDecks] = useState([]);
-    const [loading, setLoading] = useState(true);
+
+    // function to update decks once one has been added/deleted. 
+    const handleAddDeck = async (deck) => {
+        await saveDeck(deck);
+        setDecks([...decks, deck])
+    } 
+  
     const effect = async () => {
-        try {
-            setDecks(await getUserDecks(auth.user.username))
-        } catch (e) {console.error(e)}
-        setLoading(false);
+      try {
+        setDecks(await getUserDecks(auth.user.username));
+        console.log("effect");
+      } catch (e) {
+        console.error(e);
+      }
     };
-
+  
     useEffect(() => {
-        effect(decks, loading)
-    }, [decks, loading]);
-
-
+      effect();
+    }, [auth.user.username]);
+  
     return (
-        <DecksContext.Provider value={{ decks, setDecks }}>
+      <DecksContext.Provider value={{ decks, setDecks, handleAddDeck }}>
         {children}
-        </DecksContext.Provider>
+      </DecksContext.Provider>
     );
-    };
-
+  };
+  
     export const useDecks = () => {
     const context = useContext(DecksContext);
 
